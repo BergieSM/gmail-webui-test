cd11d9bf8f45f426d8acb27d4cd039bd443a24bb
BergieSM <BergieSM@Gmail.com>
2016-01-07 23:00:55 -0500

    End Of Day commit, midway through second test example.

--- src/test/java/GmailSignInTest.java
+++ src/test/java/GmailSignInTest.java
@@ -18,10 +18,11 @@ import org.openqa.selenium.support.ui.WebDriverWait;
  */
 public class GmailSignInTest {
     WebDriver driver = new FirefoxDriver();
+    WebDriverWait wait = new WebDriverWait(driver,30);
 
     @Test
     public void gmailLoginShouldBeSuccessful(){
-        WebDriverWait wait = new WebDriverWait(driver,30);
+        //WebDriverWait wait = new WebDriverWait(driver,30);
         //  Go to Gmail
         driver.get("http://gmail.com");
         //  Fill in Username
@@ -60,6 +61,68 @@ public class GmailSignInTest {
         //  Verify Sign Out
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("Email")));
         Assert.assertTrue("Should find email field if signed out",driver.findElement(By.id("Email")).isDisplayed());
+    }
+
+    @Test
+    public void gmailSendAndReceiveEmailTest(){
+    //All from previous example test
+        //  Go to Gmail
+        driver.get("http://gmail.com");
+        //  Fill in Username
+        WebElement usernameTextBox = driver.findElement(By.id("Email"));
+        usernameTextBox.clear();
+        usernameTextBox.sendKeys("bergiesm@gmail.com");
+        //  Click Next
+        WebElement usernameNextButton = driver.findElement(By.id("next"));
+        usernameNextButton.click();
+        //  Fill in Password
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("Passwd")));
+        WebElement passwordTextBox = driver.findElement(By.id("Passwd"));
+        passwordTextBox.clear();
+        passwordTextBox.sendKeys("b3rgB#RG##");
+        // Uncheck 'Stay Signed In'
+        WebElement staySignedInCheckbox = driver.findElement(By.id("PersistentCookie"));
+        String staySignedInCheckedAttribute = staySignedInCheckbox.getAttribute("checked");
+        //System.out.println("staySignedInCheckedAttribute = "+staySignedInCheckedAttribute);   DEBUG
+        //System.out.println(staySignedInCheckedAttribute); DEBUG
+        if(staySignedInCheckedAttribute.equals("true")){
+            staySignedInCheckbox.click();
+            //staySignedInCheckedAttribute =  staySignedInCheckbox.getAttribute("checked"); DEBUG
+            //System.out.println("staySignedInCheckedAttribute after click = "+staySignedInCheckedAttribute);   DEBUG
+        }
+        //  Click Sign In
+        WebElement signInButton = driver.findElement(By.id("signIn"));
+        signInButton.click();
+        //  Verify Sign In
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.partialLinkText("Inbox")));
+        Assert.assertTrue("Inbox should exist",driver.findElement(By.partialLinkText("Inbox")).getSize().getWidth()>0);
+    //New content here
+        //  Click Compose
+        WebElement composeButton = driver.findElement(By.cssSelector("div[role='button'][gh='cm']"));
+        composeButton.click();
+        //  Fill in Recipient
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(":cw")));
+        WebElement emailRecipientTextArea = driver.findElement(By.cssSelector("textarea[id=':cw']"));
+        emailRecipientTextArea.clear();
+        emailRecipientTextArea.sendKeys("BergieSM@Gmail.com");
+        //  Fill in Subject
+        WebElement emailSubjectTextEntry = driver.findElement(By.cssSelector("textarea[id=':db']"));
+        emailSubjectTextEntry.clear();
+        emailSubjectTextEntry.sendKeys("SUBJECT HERE");
+        //  Fill in Email Body
+        WebElement emailBodyTextEntry = driver.findElement(By.cssSelector("textarea[id=':gy']"));
+        emailBodyTextEntry.clear();
+        emailBodyTextEntry.sendKeys("EMAIL BODY HERE");
+        //  Click Send
+        WebElement emailSendButton = driver.findElement(By.cssSelector("div[role='button'][id=':dl']"));
+        emailSendButton.click();
+        //  Click Inbox Again
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.partialLinkText("Inbox")));
+        driver.findElement(By.partialLinkText("Inbox")).click();
+        //  Click Email
+
+        //  Verify the email subject and body are both correct
+        //  Sign out
     }
 
     @After
